32071fba3a4e88fc38ca25a2a857a8f9e3633804
HarshitChaurasia31 <harshitchaurasia153@gmail.com>
2026-08-11 21:03:53 +0530

    Improved organisation and fixed email service

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -1,24 +1,27 @@
-const nodemailer=require('nodemailer')
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 
-const transporter=nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        type:'OAUTH2',
-        user:process.env.EMAIL_USER,
-        clientId:process.env.CLIENT_ID,
-        clientSecret:process.env.CLIENT_SECRET,
-        refreshToken:process.env.REFRESH_TOKEN,
-    },
-})
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
 
-transporter.verify((error,success)=>{
-    if(error){
-        console.error('Error connecting to gmail server:',error)
-    }
-    else{
-        console.log('Email server is ready to send messages')
-    }
-})
+// Verify the connection configuration
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Error connecting to email server:', error);
+  } else {
+    console.log('Email server is ready to send messages');
+  }
+});
+
+module.exports = transporter;
 
 
 // Function to send email
